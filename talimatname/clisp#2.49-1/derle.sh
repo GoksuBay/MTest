2ff6a17c0edcd98079623aad05f30b32a622a470
unknown <goksu1.9.9.8@gmail.com>
2019-08-20 13:48:07 +0300

    drtyu

---
+++ talimatname/clisp#2.49-1/derle.sh
@@ -0,0 +1,12 @@
+ulimit -s 16384
+
+	./configure \
+		--prefix=/usr \
+		--with-ffcall \
+		--with-dynamic-ffi \
+		--without-dynamic-modules \
+		--disable-mmap \
+		--mandir=/usr/share/man \
+		--infodir=/usr/share/info \
+		"$builddir"
+	make -j1
