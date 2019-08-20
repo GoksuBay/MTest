bbb338cd8f58accd513d1a5dbe61fa8642308a8b
unknown <goksu1.9.9.8@gmail.com>
2019-08-20 15:08:14 +0300

    Update derle.sh

--- talimatname/clisp#2.49-1/derle.sh
+++ talimatname/clisp#2.49-1/derle.sh
@@ -1,4 +1,3 @@
 ulimit -s 16384
-./configure --prefix=/usr --with-ffcall --with-dynamic-ffi --without-dynamic-modules --disable-mmap --mandir=/usr/share/man --infodir=/usr/share/info \
-		"$BUILD"
+./configure --prefix=/usr --with-ffcall --with-dynamic-ffi --without-dynamic-modules --disable-mmap --mandir=/usr/share/man --infodir=/usr/share/info "$builddir"
 	make -j1
