92e449b327113f27d2d427c8711e0529aae5bb62
unknown <goksu1.9.9.8@gmail.com>
2019-08-20 14:52:17 +0300

    Update derle.sh

--- talimatname/clisp#2.49-1/derle.sh
+++ talimatname/clisp#2.49-1/derle.sh
@@ -1,3 +1,16 @@
 ulimit -s 16384
+
+	./configure \
+		--prefix=/usr \
+		--with-ffcall \
+		--with-dynamic-ffi \
+		--without-dynamic-modules \
+		--disable-mmap \
+		--mandir=/usr/share/man \
+		--infodir=/usr/share/info \
+		"$BUILD"
 	cd src
+	vi config.lisp
 	make
+	make check
+	make -j1
