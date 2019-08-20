a85b167a0725d2789b0ad49b17aabbaddbb82261
unknown <goksu1.9.9.8@gmail.com>
2019-08-20 11:27:05 +0300

    l

---
+++ talimatname/sysstat#11.6.0/derle.sh
@@ -0,0 +1,10 @@
+	./configure \
+		--build=$CBUILD \
+		--host=$CHOST \
+		--prefix=/usr \
+		--sysconfdir=/etc \
+		--mandir=/usr/share/man \
+		--infodir=/usr/share/info \
+		--disable-nls \
+		|| return 1
+	make || return 1
