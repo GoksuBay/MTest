07eb158e338e840452fb41cbc5de2470d2fe08d9
unknown <goksu1.9.9.8@gmail.com>
2019-08-21 12:56:14 +0300

    Update derle.sh

--- talimatname/clisp#2.49-1/derle.sh
+++ talimatname/clisp#2.49-1/derle.sh
@@ -1,12 +1,9 @@
-ulimit -s 16384
-	./configure \
-		--prefix=/usr \
-		--with-ffcall \
-		--with-dynamic-ffi \
-		--without-dynamic-modules \
-		--disable-mmap \
-		--mandir=/usr/share/man \
-		--infodir=/usr/share/info \
-		"$SRC"
-	cd /tmp/work/src
-	make
+sed -i 's|(:name "rl_readline_state") (:type int)|(:name "rl_readline_state") (:type ulong)|g' modules/readline/readline.lisp
+  
+  ./configure --prefix=/usr --with-readline --with-ffcall src
+  
+  cd src
+  ./makemake --prefix=/usr --with-readline --with-ffcall --with-dynamic-ffi > Makefile
+  make 
+  sed -i 's|http://www.lisp.org/HyperSpec/|http://www.lispworks.com/reference/HyperSpec/|g' config.lisp
+  make 
