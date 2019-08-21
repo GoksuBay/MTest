0b58695cb9989f949b9240637f294d1bfe157c15
unknown <goksu1.9.9.8@gmail.com>
2019-08-21 11:46:20 +0300

    Update derle.sh

--- talimatname/clisp#2.49-1/derle.sh
+++ talimatname/clisp#2.49-1/derle.sh
@@ -1,6 +1,5 @@
-cd $isim
   ./configure --prefix=/usr --with-readline --with-ffcall src
 
-  cd src
+  cd sources
   ./makemake --prefix=/usr --with-readline --with-ffcall --with-dynamic-ffi > Makefile
   make
