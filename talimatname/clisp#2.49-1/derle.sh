15e27e55637db5b5c49db41a5983e83dcec82afb
unknown <goksu1.9.9.8@gmail.com>
2019-08-21 12:10:18 +0300

    Update derle.sh

--- talimatname/clisp#2.49-1/derle.sh
+++ talimatname/clisp#2.49-1/derle.sh
@@ -1,12 +1,3 @@
 ulimit -s 16384
 cd /tmp/work/src
-	./configure \
-		--prefix=/usr \
-		--with-ffcall \
-		--with-dynamic-ffi \
-		--without-dynamic-modules \
-		--disable-mmap \
-		--mandir=/usr/share/man \
-		--infodir=/usr/share/info \
-		"$SRC"
-	make -j1
+	make 
